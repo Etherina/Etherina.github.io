37201597f94b5065e4cb9d8f09bf5c88cffaea86
Aryanstha <aryanstha4859@gmail.com>
2023-07-31 22:12:56 +0545

    initial
    s

---
+++ assets/js/script.js
@@ -0,0 +1,77 @@
+(function ($) {
+    "use strict"
+
+	/* Document on load functions */
+	$(window).on('load', function () {
+        preLoader();
+		headerHeightFixer();
+    });
+	/* Document on Resize functions */
+	$(window).on('resize', function () {
+		headerHeightFixer();
+    });
+
+	/* Preloader init */
+	function preLoader(){
+		$(".preloader").delay(1000).fadeOut("slow");
+	}
+
+	/* Fixed Header */
+	$(window).on("scroll", function () {
+		var scrolling = $(this).scrollTop();
+
+		if (scrolling > $('.header').innerHeight()) {
+			$(".header").addClass("header--fixed");
+		} else {
+			$(".header").removeClass("header--fixed");
+		}
+	});
+
+	/* scroll top */
+	$(".scroll-top").on("click", function () {
+		$("html,body").animate({scrollTop: 0},50);
+	});
+	$(window).on("scroll", function () {
+		var scrolling = $(this).scrollTop();
+
+		if (scrolling > 200) {
+			$(".scroll-top").fadeIn();
+		} else {
+			$(".scroll-top").fadeOut();
+		}
+	});
+
+	/* Fix Header Height function */
+	$(document).ready(function () {
+		$('header').before('<div class="header-height-fix"></div>');
+	});
+    function headerHeightFixer(){
+    	$('.header-height-fix').css('height', $('header').outerHeight() +'px');
+	};
+
+	/* Closes responsive menu when a navbar link is clicked */
+	$(".nav-link, .dropdown-item").on("click", function (e) {
+		if( $(this).hasClass("dropdown-toggle") ){
+			e.preventDefault();
+		}else{
+			$(".navbar-collapse").collapse("hide");
+			$("html").removeClass("overflow-hidden");
+			$('.offCanvasMenuCloser').removeClass('show');
+		}
+	});
+	$('.navbar-toggler').on('click', function () {
+        $("html").toggleClass('overflow-hidden');
+        $('.offCanvasMenuCloser').toggleClass('show');
+    });
+    $('.offCanvasMenuCloser').on('click', function () {
+        $(this).removeClass('show');
+        $("html").removeClass("overflow-hidden");
+    });
+
+    /* veno box */
+    $('.venobox').venobox({
+        bgcolor: '#ffffff',
+        spinner: 'three-bounce',
+    });
+
+})(jQuery);
